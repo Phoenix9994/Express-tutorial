ec51b35f2cf88c0b42b03c503f3fee84a5e97946
Phoenix Keck <Phoenixrocks995@gmail.com>
2024-10-01 17:41:00 -0400

    fnishing setup

---
+++ app.js
@@ -0,0 +1,54 @@
+/*
+Expresss adds a layer ontop of js gives ability to do backend roundsg
+get requests from front end
+
+Express Routes most  important
+
+
+const Varaiable= require('./routes/authRoutes.js');
+
+get- get resources( frontend) ex Data & Assets
+, post- Frnt end sending back end actions (Update database , ezecute actions- request to change resources)
+
+, delete request- Delete in data base
+
+If second middle war then 3rd is fnction to exec
+Express- ex syntx
+ router.post((NAME OF ROUTE), Either(Function to Exec  MiddleWare), (Funct to exec- only if mid is middleware)  )
+
+ Middle ware- Layer between frontend and backend
+ - Attaching middle ware to route will attach front end and pass through middle ware to connect to backend 
+
+ mkdir Project -creates a directory known as projects
+git config --global user.email "Phoenixrocks995@gmail.com"
+ssh-add C:\Users\phoen/.ssh/id_ed25519
+Start-Service ssh-agent
+Get-Service -Name ssh-agent | Set-Service -StartupType Manual
+
+
+echo "# Express Tutorial" >> README.md  - Creates a readme
+git add . - Updares all folders
+git status - Status on files
+git commit -m "adding readme file" - adds a commit to github
+git push origin main - Updates folder
+
+U- Untracked
+A- Tracked
+name/ - / indicates directory
+.name -indicates file
+Side note: Saturday HackRPI tutorial
+---------------------------------------------------
+Notes for Setting UP and development:
+Initialize Project
+    npm init -y
+Setting Up A Project 
+    mkdir PROJECT_DIRECTORY - Creates a Directory
+    
+
+Install Environment: (express, mongoose, dotenv)
+    npm install express mongoose dotenv
+    npm install --sav-dev nodemon - Automatically Restart the server on changes
+
+
+
+*/
